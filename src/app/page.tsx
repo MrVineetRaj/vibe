f09cfa5376cb577ab4a84048ca4c19a89ab61b46
MrVineetRaj <vineetrajrj26@gmail.com>
2025-07-06 15:19:10 +0530

    Inngest Setup finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,51 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { toast } from "sonner";
 
 const HomePage = () => {
-  const trpc = useTRPC();
-  const greeting = useQuery(trpc.createAI.queryOptions({ text: "test world" }));
+  const [value, setValue] = useState<string>("");
 
-  return <div>{greeting.data?.greeting}</div>;
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Function invoked");
+      },
+    })
+  );
+
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <form
+        action=""
+        className="flex flex-col gap-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <Input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <Button
+          onClick={() => {
+            invoke.mutate({
+              value: value,
+            });
+          }}
+          disabled={invoke.isPending}
+        >
+          Invoke Background
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default HomePage;
